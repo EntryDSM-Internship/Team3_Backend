33d063752d6fb73b21038aab31cadd86f1549da8
dkssud9556 <dsmeodnd123@gmail.com>
2019-12-04 19:16:12 +0900

    profile image

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,4 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const jwt = require('../jwt');
+const User = require('../models').User;
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function(req, file, cb) {
+            cb(null, 'profileImgs/');
+        },
+        filename: function(req, file, cb) {
+            const ext = path.extname(file.originalname);
+            cb(null, path.basename(file.originalname, ext) + new Date().valueOf() + ext);
+        }
+    })
+});
+
+router.patch('/img', upload.single('profileImg'), async (req, res, next) => {
+    const token = req.get('Authorization');
+    try {
+        const user = await jwt.verify(token);
+        
+    } catch(err) {
+        next(err);
+    }
+});
 
 module.exports = router;
