029864b1e2299fc0444fffd35ff73716bdb480c3
dkssud9556 <dsmeodnd123@gmail.com>
2019-12-19 17:12:56 +0900

    middleware update

---
+++ src/middlewares/loginCheck.js
@@ -0,0 +1,14 @@
+const jwt = require('../jwt');
+
+const isLoggedIn = async (req, res, next) => {
+    const token = req.get('Authorization');
+    try {
+        const decoded = await jwt.verify(token);
+        req.decoded = decoded;
+        next();
+    } catch(err) {
+        next(err);
+    }
+}
+
+module.exports = {isLoggedIn};
