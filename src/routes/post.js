0ad9a61136e7308be8ed6bc0d0cfaedd5390a1f2
dkssud9556 <dsmeodnd123@gmail.com>
2019-12-19 18:12:29 +0900

    jwt module remove

--- src/routes/post.js
+++ src/routes/post.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
-const jwt = require('../jwt');
 const fs = require('fs');
 const {isLoggedIn} = require('../middlewares/loginCheck');
 const {Post, PostImgs, Comment, User} = require('../models');
